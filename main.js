8d785e81bcf0b995780154c0bf20947e31b01f9b
EthanKlausmeyer <erklausmeyer@mail.fhsu.edu>
2022-09-19 23:12:10 -0500

    Project Finished
    - Final upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// Question 1
+
+let myNumber;
+
+// Question 2
+
+myNumber = 5;
+
+// Question 3
+
+let myOtherNumber = 5;
+
+// Question 4
+
+let myTotal;
+
+myTotal = myNumber + myOtherNumber;
+
+// Question 5
+
+let myName = "Ethan";
+
+let myCombo = myNumber.toString() + myOtherNumber.toString(); //I use toString() to convert both number variables to strings so myCombo is the correct string type.
+
+// Question 6
+
+let myOtherCombo = myNumber + myOtherNumber;
+
+// Question 7
+
+let myRemainder;
+
+myRemainder = myNumber % 5;
+
+// Question 8
+
+let i = 2;
+
+// Question 9
+
+let int = 4;
+
+// Question 10
+
+let myBoolean = false;
